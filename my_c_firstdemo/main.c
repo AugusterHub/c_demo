a8ef3bd621d8ff6e4f65c539ff581a0c4d73a3f2
zhaipeng <zhaipeng@topscomm.com>
2021-01-14 21:17:22 +0800

    数据类型sizeof

--- my_c_firstdemo/main.c
+++ my_c_firstdemo/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add(int a, int b)
+/*int add(int a, int b)
 {
     return a * b;
 }
@@ -12,6 +12,29 @@ int main(void)
     i = 2;
     i = 10;
     i = add(3, 4);
-    printf("i = %d\n故障范围内", i);
+    printf("i = %d\n", i);
     return 0;
+}*/
+
+// c 数据类型所占的字节数
+int main()
+{
+    char ch;
+    int a;
+    short b;
+    long c;
+    float d;
+    double e;
+    ch = 'a';
+    a = 1;
+    b = 2;
+    c = 0;
+    d = 1.5;
+    e = 1.5;
+    printf("%d\n", sizeof(ch));
+    printf("%d\n", sizeof(a));
+    printf("%d\n", sizeof(b));
+    printf("%d\n", sizeof(c));
+    printf("%d\n", sizeof(d));
+    printf("%d\n", sizeof(e));
 }
