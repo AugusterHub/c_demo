eadca89f5eeb5f9e3da292c94b495b698c85a4e7
zhaipeng <zhaipeng@topscomm.com>
2021-01-15 16:31:10 +0800

    数据运算

--- my_c_firstdemo/main.c
+++ my_c_firstdemo/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// 1 变量与常量
 /*int add(int a, int b)
 {
     return a * b;
@@ -16,8 +16,8 @@ int main(void)
     return 0;
 }*/
 
-// c 数据类型所占的字节数
-int main()
+// c 变量&数据类型所占的字节数
+/*int main()
 {
     char ch;
     int a;
@@ -37,4 +37,30 @@ int main()
     printf("%d\n", sizeof(c));
     printf("%d\n", sizeof(d));
     printf("%d\n", sizeof(e));
-}
+}*/
+
+// 常量（宏常量，const常量）
+/*#define PI 3.14
+#define R 10
+main()
+{
+    printf("squere = %f\n", PI * R * R);
+}*/
+/*main()
+{
+    const float PI = 3.14;
+    const float R = 10;
+    printf("squere = %f\n", PI * R * R);
+}*/
+
+
+ //赋值运算
+ int main()
+ {
+     int a;
+     float x, y;
+     a = 0;
+     x = y = 1.5;
+     printf("%d\n%f\n%f\n",a, x, y);
+     return 0;
+ }
